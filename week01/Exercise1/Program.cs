dfb901bf6ddf8b649bfd32cf0985e4abd6ad929b
cher9804 <cristianhernandezcastro98@gmail.com>
2025-01-18 11:10:43 -0700

    Ex 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Console.WriteLine("This is in C#");
+        // int number = 5;
+        // number = 8;
+        // number = number + 3;
+        // // string color = "blue";
+        // Console.WriteLine("What is your favorite colore? ");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"Your color is {color}");
+        // // if (number > 3 )
+        // // {
+        // //     Console.WriteLine("");
+        // // }
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+        
+        Console.WriteLine($"Youe name is {lname}, {fname} {lname}.");
+
+        
+
+
     }
 }
